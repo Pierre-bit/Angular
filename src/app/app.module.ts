f08667cdc7b60cc8edebd5b8b2f3ad1f52cac844
Pierre-bit <jeanneaupierre@gmail.com>
2022-07-29 17:06:42 +0200

    modify app-component.* et add search-box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule
